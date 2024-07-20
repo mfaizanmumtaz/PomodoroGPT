ba41eec5b5d4714fab6d1279b00aa7f531ec4154
mfaizanmumtaz <mfaizanmumtaz999@gmail.com>
2024-07-20 17:39:07 +0500

    my first commit

--- app/pomodorogpt.py
+++ app/pomodorogpt.py
@@ -1,3 +1,6 @@
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain.schema.output_parser import StrOutputParser
 from deepgram import DeepgramClient, SpeakOptions
 from langchain_core.prompts import PromptTemplate
@@ -5,17 +8,17 @@ from langchain_groq import ChatGroq
 from playsound import playsound
 import time
 
-DEEPGRAM_API_KEY = "Your Deepgram API Key"
+DEEPGRAM_API_KEY = "your deepgram api key"
 
 
 class PomodoroGpt:
+    store = {}
+
     def __init__(self, topic: str) -> None:
         self.topic = topic
 
     def reminder(self, type: str) -> None:
-        playsound(
-            f"/home/faizan/Documents/PomodoroGPT/reminder_sounds/{type}"
-        )
+        playsound(f"/home/faizan/Documents/PomodoroGPT/reminder_sounds/{type}")
 
     def text_to_voice(self, TEXT: str) -> None:
         try:
@@ -25,22 +28,31 @@ class PomodoroGpt:
                 model="aura-asteria-en",
             )
             FILENAME = "/home/faizan/Documents/PomodoroGPT/reminder_sounds/audio.mp3"
-            deepgram.speak.v("1").save(FILENAME, {"text":TEXT}, options)
+            deepgram.speak.v("1").save(FILENAME, {"text": TEXT}, options)
             playsound(FILENAME)
 
         except Exception as e:
             print(f"Exception: {e}")
 
-    def generate_quiz(self, topic: str) -> None:
-        prompt = (
+    def get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = InMemoryChatMessageHistory()
+        return self.store[session_id]
+
+    def generate_quiz(self) -> None:
+        chain = (
             PromptTemplate.from_template(
                 """Please generate a only one unique quiz on that topic. The quiz should contain concise question and four options. Please do not generate a lengthy quiz; keep it as short as possible. {topic}"""
             )
             | ChatGroq(model_name="mixtral-8x7b-32768")
             | StrOutputParser()
         )
-
-        self.text_to_voice(prompt.invoke({"topic": topic}))
+        wrapped_chain = RunnableWithMessageHistory(chain, self.get_session_history)
+        self.text_to_voice(
+            wrapped_chain.invoke(
+                self.topic, config={"configurable": {"session_id": "abc123"}}
+            )
+        )
 
     def pomodoro_timer(self) -> None:
         """Implements a Pomodoro timer with 25 minutes work and 5 minutes break."""
@@ -56,9 +68,9 @@ class PomodoroGpt:
             print("Time for a break!")
             time.sleep(300)  # 5 minutes in seconds
             self.reminder("breaktimeendandquizreminder.mp3")
-            self.generate_quiz(self.topic)
+            self.generate_quiz()
 
 
-user1 = PomodoroGpt("autoencoders")
+user1 = PomodoroGpt(input("Enter topic:"))
 user1.pomodoro_timer()
 # user1.text_to_voice("my name is Muhammad.")
